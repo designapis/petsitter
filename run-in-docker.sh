4f753b63ee932008fd237bcc5191896ce126c459
Josh Ponelat <jponelat@gmail.com>
2020-05-09 15:20:43 +0200

    add docker-compose file

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,8 +1,4 @@
 # Build Container
-docker build -t petsitter-backend .
-
-# Start Dependencies
-sh run-database.sh
-
-# Run Container
-docker run -p 8080:8080 --link=petsitter-db:petsitter-db petsitter-backend
+docker build -t designapis/petsitter:latest .
+# Run stack
+docker-compose -f ./docker-compose.yml up -d
