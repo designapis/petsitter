8680bd0e22691ec700b5b0bca4c64affdc2bea14
Josh Ponelat <jponelat@gmail.com>
2021-06-02 11:51:04 +0200

    use localhost as db url default instead of docker container

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,7 @@ const express = require('express');
 const oas3Tools = require('oas3-tools');
 const serverPort = 8080;
 
-//const databaseUrl = 'mongodb://127.0.0.1/petsitter_db';
-const databaseUrl = process.env.DB_URL || 'mongodb://petsitter-db/petsitter_db';
+const databaseUrl = process.env.DB_URL || 'mongodb://127.0.0.1/petsitter_db';
 const mongoose = require('mongoose');
 
 // swaggerRouter configuration
